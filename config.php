db139374f870842cfa4171ebe580bc0a3176a0f4
Lukas Velek <lukas.velek@centrum.cz>
2023-11-04 23:34:04 +0100

    Revision 98

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ $cfg['db_pass'] = '';
 $cfg['db_name'] = '';
 
 $cfg['log_dir'] = '';
-$cfg['log_level'] = '3'; // 0 - no log, 1 - log error only, 2 - log error & warning only, 3 - log all 
-$cfg['sql_log_level'] = '1'; // 0 - no log, 0 - log all
+$cfg['log_level'] = 0; // 0 - no log, 1 - log error only, 2 - log error & warning only, 3 - log all 
+$cfg['sql_log_level'] = 0; // 0 - no log, 1 - log all
 
 $cfg['cache_dir'] = '';
 $cfg['serialize_cache'] = true; // false does not work yet
