7d6733e62a7ae471fef448cf25aa14ca80f5b5e2
Lukas Velek <lukas.velek@centrum.cz>
2023-11-04 23:23:02 +0100

    Revision 96

--- app/modules/UserModule/presenters/SingleDocument.php
+++ app/modules/UserModule/presenters/SingleDocument.php
@@ -7,6 +7,7 @@ use DMS\Core\TemplateManager;
 use DMS\Entities\Document;
 use DMS\Modules\APresenter;
 use DMS\Modules\IModule;
+use DMS\UI\FormBuilder\FormBuilder;
 use DMS\UI\LinkBuilder;
 use DMS\UI\TableBuilder\TableBuilder;
 
@@ -53,6 +54,274 @@ class SingleDocument extends APresenter {
         return $template;
     }
 
+    protected function showEdit() {
+        global $app;
+
+        $id = htmlspecialchars($_GET['id']);
+        $document  = $app->documentModel->getDocumentById($id);
+
+        $template = $this->templateManager->loadTemplate('app/modules/UserModule/presenters/templates/documents/new-document-form.html');
+
+        $data = array(
+            '$PAGE_TITLE$' => 'Edit document <i>' . $document->getName() . '</i>',
+            '$NEW_DOCUMENT_FORM$' => $this->internalCreateDocumentEditForm($document)
+        );
+
+        $this->templateManager->fill($data, $template);
+
+        return $template;
+    }
+
+    protected function updateDocument() {
+        global $app;
+
+        $id = htmlspecialchars($_GET['id']);
+
+        $data = [];
+
+        $idGroup = htmlspecialchars($_POST['group']);
+        $idFolder = htmlspecialchars($_POST['folder']);
+        
+        $data['name'] = htmlspecialchars($_POST['name']);
+        $data['id_manager'] = htmlspecialchars($_POST['manager']);
+        $data['status'] = htmlspecialchars($_POST['status']);
+        $data['id_group'] = htmlspecialchars($idGroup);
+
+        if($idFolder != '-1') {
+            $data['id_folder'] = $idFolder;
+        }
+
+        unset($_POST['name']);
+        unset($_POST['manager']);
+        unset($_POST['status']);
+        unset($_POST['group']);
+        unset($_POST['folder']);
+
+        $customMetadata = $_POST;
+
+        $data = array_merge($data, $customMetadata);
+
+        $app->documentModel->updateDocument($id, $data);
+
+        $app->redirect('UserModule:SingleDocument:showInfo', array('id' => $id));
+    }
+
+    private function internalCreateDocumentEditForm(Document $document) {
+        global $app;
+
+        $idFolder = $document->getIdFolder();
+
+        $managers = array(
+            array(
+                'value' => $app->user->getId(),
+                'text' => '&lt;&lt;Me&gt;&gt;'
+            )
+        );
+
+        $users = $app->userModel->getAllUsers();
+
+        foreach($users as $user) {
+            $manager = array(
+                'value' => $user->getId(),
+                'text' => $user->getFullname()
+            );
+
+            if($document->getIdManager() == $user->getId()) {
+                $manager['selected'] = 'selected';
+            }
+
+            $managers[] = $manager;
+        }
+
+        $statusMetadata = $app->metadataModel->getMetadataByName('status', 'documents');
+        $dbStatuses = $app->metadataModel->getAllValuesForIdMetadata($statusMetadata->getId());
+
+        $statuses = [];
+        foreach($dbStatuses as $dbs) {
+            $status = array(
+                'value' => $dbs->getValue(),
+                'text' => $dbs->getName()
+            );
+
+            if($document->getStatus() == $dbs->getValue()) {
+                $status['selected'] = 'selected';
+            }
+
+            $statuses[] = $status;
+        }
+
+        $dbGroups = $app->groupModel->getAllGroups();
+
+        $groups = [];
+        foreach($dbGroups as $dbg) {
+            $group = array(
+                'value' => $dbg->getId(),
+                'text' => $dbg->getName()
+            );
+
+            if($document->getIdGroup() == $dbg->getId()) {
+                $group['selected'] = 'selected';
+            }
+
+            $groups[] = $group;
+        }
+
+        $rankMetadata = $app->metadataModel->getMetadataByName('rank', 'documents');
+        $dbRanks = $app->metadataModel->getAllValuesForIdMetadata($rankMetadata->getId());
+
+        $ranks = [];
+        foreach($dbRanks as $dbr) {
+            $rank = array(
+                'value' => $dbr->getValue(),
+                'text' => $dbr->getName()
+            );
+
+            if($document->getRank() == $dbr->getValue()) {
+                $rank['selected'] = 'selected';
+            }
+
+            $ranks[] = $rank;
+        }
+
+        $dbFolders = $app->folderModel->getAllFolders();
+
+        $folders = [];
+        $folders[] = array(
+            'value' => '-1',
+            'text' => '-'
+        );
+
+        foreach($dbFolders as $dbf) {
+            $text = $dbf->getName();
+
+            for($i = 0; $i < $dbf->getNestLevel(); $i++) {
+                $text = '&nbsp;&nbsp;' . $text;
+            }
+
+            $folder = array(
+                'value' => $dbf->getId(),
+                'text' => $text
+            );
+
+            if($document->getIdFolder() == $dbf->getId()) {
+                $folder['selected'] = 'selected';
+            }
+
+            $folders[] = $folder;
+        }
+
+        $customMetadata = $app->metadataModel->getAllMetadataForTableName('documents');
+        // name = array('text' => 'text', 'options' => 'options from metadata_values')
+        $metadata = [];
+
+        foreach($customMetadata as $cm) {
+            if($cm->getIsSystem()) {
+                continue;
+            }
+
+            $name = $cm->getName();
+            $text = $cm->getText();
+            $values = $app->metadataModel->getAllValuesForIdMetadata($cm->getId());
+
+            $options = [];
+            foreach($values as $v) {
+                $option = array(
+                    'value' => $v->getValue(),
+                    'text' => $v->getName()
+                );
+                
+                if(!is_null($document->getMetadata($name))) {
+                    $option['selected'] = 'selected';
+                }
+
+                $options[] = $option;
+            }
+
+            $metadata[$name] = array('text' => $text, 'options' => $options, 'type' => $cm->getInputType(), 'length' => $cm->getInputLength());
+        }
+
+        $fb = FormBuilder::getTemporaryObject();
+
+        $fb ->setMethod('POST')->setAction('?page=UserModule:SingleDocument:updateDocument&id=' . $document->getId())
+            ->addElement($fb->createLabel()->setText('Document name')
+                                           ->setFor('name'))
+            ->addElement($fb->createInput()->setType('text')
+                                           ->setName('name')
+                                           ->setValue($document->getName())
+                                           ->require())
+            ->addElement($fb->createLabel()->setText('Manager')
+                                           ->setFor('manager'))
+            ->addElement($fb->createSelect()->setName('manager')
+                                            ->addOptionsBasedOnArray($managers))
+            ->addElement($fb->createLabel()->setText('Status')
+                                           ->setFor('status'))
+            ->addElement($fb->createSelect()->setName('status')
+                                            ->addOptionsBasedOnArray($statuses))
+            ->addElement($fb->createLabel()->setText('Group')
+                                           ->setFor('group'))
+            ->addElement($fb->createSelect()->setName('group')
+                                            ->addOptionsBasedOnArray($groups))
+            ->addElement($fb->createLabel()->setFor('rank')
+                                           ->setText('Rank'))
+            ->addElement($fb->createSelect()->setName('rank')
+                                            ->addOptionsBasedOnArray($ranks))
+            ->addElement($fb->createLabel()->setFor('folder')
+                                           ->setText('Folder'))
+            ->addElement($fb->createSelect()->setName('folder')
+                                            ->addOptionsBasedOnArray($folders))
+            
+           ;
+
+        foreach($metadata as $name => $d) {
+            $text = $d['text'];
+            $options = $d['options'];
+            $inputType = $d['type'];
+            $inputLength = $d['length'];
+
+            $fb->addElement($fb->createLabel()->setText($text)->setFor($name));
+
+            switch($inputType) {
+                case 'select':
+                    $fb ->addElement($fb->createSelect()->setName($name)->addOptionsBasedOnArray($options));
+                    
+                    break;
+
+                case 'text':
+                    if($inputLength > 256) {
+                        $fb->addElement($fb->createTextArea()->setName($name));
+                    } else {
+                        $fb->addElement($fb->createInput()->setType($inputType)->setMaxLength($inputLength)->setName($name));
+                    }
+
+                    break;
+
+                case 'number':
+                    $fb ->addElement($fb->createInput()->setType($inputType)->setMaxLength($inputLength)->setName($name));
+
+                    break;
+
+                case 'boolean':
+                    $fb ->addElement($fb->createInput()->setType('checkbox')->setName($name));
+
+                    break;
+
+                case 'date':
+                    $fb ->addElement($fb->createInput()->setType('date')->setName($name));
+
+                    break;
+
+                case 'datetime':
+                    $fb ->addElement($fb->createInput()->setType('datetime')->setName($name));
+
+                    break;
+            }
+        }
+
+        $fb->addElement($fb->createSubmit('Create'));
+
+        return $fb->build();
+    }
+
     private function internalCreateDocumentInfoGrid(Document $document) {
         global $app;
 
@@ -68,13 +337,20 @@ class SingleDocument extends APresenter {
             }
         }
 
+        $folder = '-';
+
+        if($document->getIdFolder() != null) {
+            $folder = $this->createFolderLink($document->getIdFolder());
+        }
+
         $data = array(
             'Name' => $document->getName(),
             'Author' => $this->createUserLink($document->getIdAuthor()),
             'Manager' => $this->createUserLink($document->getIdManager()),
             'Status' => $status,
             'Group' => $this->createGroupLink($document->getIdGroup()),
-            'Deleted?' => $document->getIsDeleted() ? 'Yes' : 'No'
+            'Deleted?' => $document->getIsDeleted() ? 'Yes' : 'No',
+            'Folder' => $folder
         );
 
         foreach($document->getMetadata() as $k => $v) {
@@ -164,6 +440,14 @@ class SingleDocument extends APresenter {
         $group = $app->groupModel->getGroupById($id);
 
         return LinkBuilder::createAdvLink(array('page' => 'UserModule:Groups:showUsers', 'id' => $id), $group->getName());
+    }
+
+    private function createFolderLink(int $id) {
+        global $app;
+
+        $folder = $app->folderModel->getFolderById($id);
+
+        return LinkBuilder::createAdvLink(array('page' => 'UserModule:Settings:showFolders', 'id' => $id), $folder->getName());
     }
 }
 
