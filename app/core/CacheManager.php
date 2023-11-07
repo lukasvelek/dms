951cf519c81e8baa154ebea033b8e2bd90b30106
Lukas Velek <lukas.velek@centrum.cz>
2023-11-07 21:59:11 +0100

    Revision 115
    Added CacheManager comments

--- app/core/CacheManager.php
+++ app/core/CacheManager.php
@@ -2,11 +2,22 @@
 
 namespace DMS\Core;
 
+/**
+ * CacheManager allows the application to cache data
+ * 
+ * @author Lukas Velek
+ */
 class CacheManager {
     private FileManager $fm;
     private bool $serialize;
     private string $category;
     
+    /**
+     * The CacheManager constructor
+     * 
+     * @param bool $serialize True if cache should be serialized and false if not
+     * @param string $category Cache category
+     */
     public function __construct(bool $serialize, string $category) {
         $this->fm = new FileManager('logs/', 'cache/');
 
@@ -14,6 +25,13 @@ class CacheManager {
         $this->category = $category;
     }
 
+    /**
+     * Saves a bulk action right to cache
+     * 
+     * @param int $idUser ID user
+     * @param string $key Bulk action name
+     * @param int $value 1 if bulk action right is allowed and 0 if not
+     */
     public function saveBulkActionRight(int $idUser, string $key, int $value) {
         $cacheData = $this->loadFromCache();
 
@@ -22,6 +40,13 @@ class CacheManager {
         $this->saveToCache($cacheData);
     }
 
+    /**
+     * Loads the bulk action right from cache
+     * 
+     * @param int $idUser ID user
+     * @param string $key Bulk action name
+     * @return bool|null True if bulk action right is allowed, false if it is not allowed, null if the entry does not exist
+     */
     public function loadBulkActionRight(int $idUser, string $key) {
         $cacheData = $this->loadFromCache();
 
@@ -42,6 +67,13 @@ class CacheManager {
         }
     }
 
+    /**
+     * Saves a panel right to cache
+     * 
+     * @param int $idUser ID user
+     * @param string $key Panel name
+     * @param int $value 1 if panel right is allowed and 0 if not
+     */
     public function savePanelRight(int $idUser, string $key, int $value) {
         $cacheData = $this->loadFromCache();
 
@@ -50,6 +82,13 @@ class CacheManager {
         $this->saveToCache($cacheData);
     }
 
+    /**
+     * Loads the panel right from cache
+     * 
+     * @param int $idUser ID user
+     * @param string $key Panel name
+     * @return bool|null True if panel right is allowed, false if it is not allowed, null if the entry does not exist
+     */
     public function loadPanelRight(int $idUser, string $key) {
         $cacheData = $this->loadFromCache();
 
@@ -70,6 +109,13 @@ class CacheManager {
         }
     }
 
+    /**
+     * Saves a metadata right to cache
+     * 
+     * @param int $idUser ID user
+     * @param string $key Metadata name
+     * @param int $value 1 if metadata right is allowed and 0 if not
+     */
     public function saveMetadataRight(int $idUser, int $idMetadata, string $key, int $value) {
         $cacheData = $this->loadFromCache();
 
@@ -80,6 +126,13 @@ class CacheManager {
         }
     }
 
+    /**
+     * Loads the metadata right from cache
+     * 
+     * @param int $idUser ID user
+     * @param string $key Metadata name
+     * @return bool|null True if metadata right is allowed, false if it is not allowed, null if the entry does not exist
+     */
     public function loadMetadataRight(int $idUser, int $idMetadata, string $key) {
         $cacheData = $this->loadFromCache();
 
@@ -106,6 +159,11 @@ class CacheManager {
         }
     }
 
+    /**
+     * Generates a filename for the cache file
+     * 
+     * @return string Filename
+     */
     public function createFilename() {
         global $app;
 
@@ -116,6 +174,11 @@ class CacheManager {
         return $file;
     }
 
+    /**
+     * Loads data from cache
+     * 
+     * @return array $data Cache data
+     */
     private function loadFromCache() {
         $filename = $this->createFilename();
 
@@ -132,6 +195,11 @@ class CacheManager {
         return $data;
     }
 
+    /**
+     * Saves data to cache
+     * 
+     * @param array $data Data to be cached
+     */
     private function saveToCache(array $data) {
         $filename = $this->createFilename();
 
@@ -142,6 +210,12 @@ class CacheManager {
         $this->fm->writeCache($filename, $data);
     }
 
+    /**
+     * Returns a temporary object
+     * 
+     * @param string $category Cache category
+     * @return CacheManager self
+     */
     public static function getTemporaryObject(string $category) {
         global $app;
 
