57db43a1d14a1d36402b381032bb567ca666123a
Lukas Velek <lukas.velek@centrum.cz>
2023-11-14 20:02:29 +0100

    Revision 156
    Source cleanup + comments

--- app/authenticators/UserAuthenticator.php
+++ app/authenticators/UserAuthenticator.php
@@ -2,16 +2,33 @@
 
 namespace DMS\Authenticators;
 
-use DMS\Constants\CacheCategories;
 use DMS\Core\CacheManager;
 use DMS\Core\DB\Database;
 use DMS\Core\Logger\Logger;
 
+/**
+ * Authenticator for users. It allows users to login or logout into the application.
+ * 
+ * @author Lukas Velek
+ */
 class UserAuthenticator extends AAuthenticator {
+    /**
+     * Constructor for user authenticator
+     * 
+     * @param Database $db Database instance
+     * @param Logger $logger Logger instance
+     */
     public function __construct(Database $db, Logger $logger) {
         parent::__construct($db, $logger);
     }
 
+    /**
+     * Checks if passed user's credentials are valid and the user can log in
+     * 
+     * @param string $username User's username
+     * @param string $password User's password
+     * @return int|bool If user's credentials are valid and the user can log in then it returns ID of that user, if not then false is returned
+     */
     public function authUser(string $username, string $password) {
         $qb = $this->qb(__METHOD__);
         $row = $qb->select('id', 'username', 'password')
@@ -32,6 +49,11 @@ class UserAuthenticator extends AAuthenticator {
         }
     }
 
+    /**
+     * Logs out the current user
+     * 
+     * @return bool True
+     */
     public function logoutCurrentUser() {
         CacheManager::invalidateAllCache();
 
--- app/authorizators/AAuthorizator.php
+++ app/authorizators/AAuthorizator.php
@@ -6,11 +6,23 @@ use DMS\Core\DB\Database;
 use DMS\Core\Logger\Logger;
 use DMS\Entities\User;
 
+/**
+ * Abstract class that is common for all authorizators
+ * 
+ * @author Lukas Velek
+ */
 abstract class AAuthorizator {
     private Database $db;
     private Logger $logger;
     protected int $idUser;
 
+    /**
+     * Common constructor for all authorizators
+     * 
+     * @param Database $db Database instance
+     * @param Logger $logger Logger instance
+     * @param User|null $user Current user instance
+     */
     protected function __construct(Database $db, Logger $logger, ?User $user) {
         $this->db = $db;
         $this->logger = $logger;
@@ -20,7 +32,12 @@ abstract class AAuthorizator {
         }
     }
 
-    public function setIdUser($idUser) {
+    /**
+     * Sets id of the user for all authorizators
+     * 
+     * @param int $idUser ID of the user
+     */
+    public function setIdUser(int $idUser) {
         $this->idUser = $idUser;
     }
 
