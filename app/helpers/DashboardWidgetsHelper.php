49e2877c67c41db4f841f703660c534d0b29e29a
Lukas Velek <lukas.velek@centrum.cz>
2023-11-20 15:55:04 +0100

    Revision 183
    Updated DashboardWidgetsHelper

--- app/helpers/DashboardWidgetsHelper.php
+++ app/helpers/DashboardWidgetsHelper.php
@@ -3,11 +3,7 @@
 namespace DMS\Helpers;
 
 class DashboardWidgetsHelper {
-    public static function render(string $funcName) {
-        if(method_exists(self, $funcName)) {
-            self::{$funcName}();
-        }
-    }
+    
 
     public static function createTotalDocuments() {
 
