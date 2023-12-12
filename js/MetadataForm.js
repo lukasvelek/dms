0dfb33d2edd9298365d8bbc862825b0aebe9092e
Lukas Velek <lukas.velek@centrum.cz>
2023-12-12 13:21:54 +0100

    Revision 228
    Bugfixes

--- js/MetadataForm.js
+++ js/MetadataForm.js
@@ -2,39 +2,59 @@ var type = document.getElementById("input_type");
 var lengthElem = document.getElementById("length");
 
 if(type.value == "select") {
-    lengthElem.disabled = true;
-    lengthElem.value = "256";
+    //lengthElem.disabled = true;
+    lengthElem.setAttribute('readonly', true);
+    //lengthElem.value = "256";
+    lengthElem.setAttribute('value', '256');
 } else if(type.value == "boolean") {
-    lengthElem.disabled = true;
-    lengthElem.value = "2";
+    //lengthElem.disabled = true;
+    lengthElem.setAttribute('readonly', true);
+    //lengthElem.value = "2";
+    lengthElem.setAttribute('value', '2');
 } else if(type.value == "date") {
-    lengthElem.disabled = true;
-    lengthElem.value = "10";
+    //lengthElem.disabled = true;
+    lengthElem.setAttribute('readonly', true);
+    //lengthElem.value = "10";
+    lengthElem.setAttribute('value', '10');
 } else if(type.value == "datetime") {
-    lengthElem.disabled = true;
-    lengthElem.value = "16";
+    //lengthElem.disabled = true;
+    lengthElem.setAttribute('readonly', true);
+    //lengthElem.value = "16";
+    lengthElem.setAttribute('value', '16');
 } else {
-    lengthElem.disabled = false;
-    lengthElem.value = "";
+    //lengthElem.disabled = false;
+    lengthElem.setAttribute('readonly', false);
+    //lengthElem.value = "";
+    lengthElem.setAttribute('value', '');
 }
 
 type.onchange = function() {
     var value = type.value;
 
     if(value == "select") {
-        lengthElem.disabled = true;
-        lengthElem.value = "256";
+        //lengthElem.disabled = true;
+        lengthElem.setAttribute('readonly', true);
+        //lengthElem.value = "256";
+        lengthElem.setAttribute('value', '256');
     } else if(type.value == "boolean") {
-        lengthElem.disabled = true;
-        lengthElem.value = "2";
+        //lengthElem.disabled = true;
+        lengthElem.setAttribute('readonly', true);
+        //lengthElem.value = "2";
+        lengthElem.setAttribute('value', '2');
     } else if(type.value == "date") {
-        lengthElem.disabled = true;
-        lengthElem.value = "10";
+        //lengthElem.disabled = true;
+        lengthElem.setAttribute('readonly', true);
+        //lengthElem.value = "10";
+        lengthElem.setAttribute('value', '10');
     } else if(type.value == "datetime") {
-        lengthElem.disabled = true;
-        lengthElem.value = "16";
+        //lengthElem.disabled = true;
+        lengthElem.setAttribute('readonly', true);
+        //lengthElem.value = "16";
+        lengthElem.setAttribute('value', '16');
     } else {
-        lengthElem.disabled = false;
-        lengthElem.value = "";
+        //lengthElem.disabled = false;
+        lengthElem.setAttribute('readonly', false);
+        //lengthElem.value = "";
+        lengthElem.setAttribute('value', '');
     }
 };
