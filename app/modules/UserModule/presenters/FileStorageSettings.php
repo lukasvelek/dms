27fe79889bdd33cdcab8286ea4815bdde5acf283
Lukas Velek <lukas.velek@centrum.cz>
2024-02-28 18:24:00 +0100

    Code cleanup

--- app/modules/UserModule/presenters/FileStorageSettings.php
+++ app/modules/UserModule/presenters/FileStorageSettings.php
@@ -262,67 +262,6 @@ class FileStorageSettings extends APresenter {
     }
 
     private function internalCreateLocationsGrid() {
-        /*global $app;
-
-        $fsManager = $app->fsManager;
-
-        $locations = $app->fileStorageModel->getAllFileStorageLocations(true);
-        $locationCount = count($locations);
-
-        $gb = new GridBuilder();
-
-        $gb->addColumns(['order' => 'Order', 'name' => 'Name', 'path' => 'Path', 'type' => 'Type', 'isDefault' => 'Default', 'isActive' => 'Active', 'freeSpace' => 'Free space', 'fileCount' => 'Files stored']);
-        $gb->addDataSource($locations);
-        $gb->addOnColumnRender('isDefault', function(FileStorageLocation $loc) {
-            return GridDataHelper::renderBooleanValueWithColors($loc->isDefault(), 'Yes', 'No');
-        });
-        $gb->addOnColumnRender('isActive', function(FileStorageLocation $loc) {
-            return GridDataHelper::renderBooleanValueWithColors($loc->isActive(), 'Yes', 'No');
-        });
-        $gb->addOnColumnRender('freeSpace', function(FileStorageLocation $loc) use ($fsManager) {
-            return $fsManager->getFreeSpaceLeft($loc->getPath());
-        });
-        $gb->addOnColumnRender('type', function(FileStorageLocation $loc) {
-            return FileStorageTypes::$texts[$loc->getType()];
-        });
-        $gb->addOnColumnRender('fileCount', function(FileStorageLocation $loc) use ($fsManager) {
-            return count($fsManager->getStoredFilesInDirectory($loc->getPath()));
-        });
-        $gb->addAction(function(FileStorageLocation $loc) use ($locationCount) {
-            if($locationCount > 1) {
-                if($loc->getOrder() < $locationCount) {
-                    return LinkBuilder::createAdvLink(['page' => 'moveLocationDown', 'id' => $loc->getId(), 'order' => $loc->getOrder()], '&darr;');
-                }
-            } else {
-                return '-';
-            }
-        });
-        $gb->addAction(function(FileStorageLocation $loc) use ($locationCount) {
-            if($locationCount > 1) {
-                if($loc->getOrder() > 1) {
-                    return LinkBuilder::createAdvLink(['page' => 'moveLocationUp', 'id' => $loc->getId(), 'order' => $loc->getOrder()], '&uarr;');
-                }
-            } else {
-                return '-';
-            }
-        });
-        $gb->addAction(function(FileStorageLocation $loc) use ($locationCount) {
-            if($locationCount > 1 && $loc->isSystem() === FALSE) {
-                return LinkBuilder::createAdvLink(['page' => 'showRemoveLocationForm', 'id' => $loc->getId()], 'Remove');
-            } else {
-                return '-';
-            }
-        });
-        $gb->addAction(function(FileStorageLocation $loc) {
-            if($loc->isDefault() === FALSE) {
-                return LinkBuilder::createAdvLink(['page' => 'setLocationAsDefault', 'id' => $loc->getId(), 'type' => $loc->getType()], 'Set as default');
-            } else {
-                return '-';
-            }
-        });
-
-        return $gb->build();*/
-
         $code = '<script type="text/javascript">loadFileStorageLocations()</script>';
         $code .= '<div id="grid-loading"><img src="img/loading.gif" width="32" height="32"></div><table border="1"></table>';
         return $code;
