7251b9323fba6d9d13d40c8ea3c26ed38373f367
Lukas Velek <lukas.velek@centrum.cz>
2024-08-21 19:09:17 +0200

    Dependencies

---
+++ app/core/QueryBuilder/ILoggerCallable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace QueryBuilder;
+
+use Exception;
+
+/**
+ * ILoggerCallable is an interface that must be implemented by a class that allows logging.
+ * 
+ * @author Lukas Velek
+ */
+interface ILoggerCallable {
+    function sql(string $sql, string $method, ?float $msTaken);
+    function exception(Exception $e, string $method);
+}
+
+?>
